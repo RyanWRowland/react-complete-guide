91bd787e3d0e9edb096c9bd15eddf1fc3e5a6785
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-05-14 15:00:45 -0500

    Followed course lectures, module 3, up to lecture 41: Manipulating State

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Chuck', age: 56 },
+      { name: 'Ryan', age: 23 },
+      { name: 'Zach', age: 24 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    // do not mutate state directly: DO NOT this.state.item = 'new item'
+    const sorted_persons = [...this.state.persons].sort((a, b) => a.age - b.age);
+    this.setState({
+      persons: sorted_persons
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Board Games</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+      // <h1>Another Heading</h1>
+      // ^^ will not comple, must return a single element
+    );
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default App;
