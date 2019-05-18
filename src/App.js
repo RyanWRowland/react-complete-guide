c1aa0e87a497dbeaa908ce9f51b6b99d9eb88ae2
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-05-18 12:45:17 -0500

    Dynamically styled button with JavaScript and dynamicaly styled paragraph with classes

--- src/App.js
+++ src/App.js
@@ -38,7 +38,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -66,19 +67,29 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
+    }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
       // <h1>Another Heading</h1>
-      // ^^ will not comple, must return a single element
+      // ^^ will not compile, must return a single element
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
   }
