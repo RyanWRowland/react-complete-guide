56c1362890f499d9b00d15d6ba00de28e645c0e7
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-05-20 17:26:40 -0500

    Used withClass in Person component passing unknown props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
-// import withClass from '../../../hoc/withClass';
-// import styles from './Person.module.css';
+import withClass from '../../../hoc/withClass';
+import styles from './Person.module.css';
 
 // Aux here is basically the same as React.Fragment
 /*
@@ -34,4 +34,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, styles.Person);
