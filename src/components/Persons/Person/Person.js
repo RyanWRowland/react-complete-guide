8840ee2614638b91a178b322acec5442d9cb5a2f
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-05-25 13:31:25 -0500

    added prop types to Person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 import styles from './Person.module.css';
@@ -33,5 +35,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  changed: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+};
 
 export default withClass(Person, styles.Person);
